4ae53a2c29dd6bbfcada3655f0ad9bf6934234af
Paul TK Chan <gnokit@gmail.com>
2024-07-13 17:21:28 +0800

    integrate with chromadb

--- chat.py
+++ chat.py
@@ -2,32 +2,51 @@ import streamlit as st
 import ollama
 from typing import Dict, Iterable
 from config import Config
+from query_qa_pairs import query_to_context
 
 model = Config['model']
+welcome_message = """
+Hi there! I'm the Microwave Witch 🧙‍♀️. 
+I know all about your microwave oven. What question do you have for me? I'm here to help!
+"""
+sys_prompt_prefix = """
+You are an AI expert of a household microwave oven.
+If context section exists below, use it to answer the question.
+"""   
 
-def fetch_ollama_replies(chat_history: Dict) -> Iterable:
+def get_sys_prompt(context = ""): 
+    context_prompt = "" if context is None else "\n\nContext: " + context
+    return sys_prompt_prefix + context_prompt    
+
+def generate_reply(chat_history: Dict) -> Iterable:
+    query_text = chat_history[-1]['content']
+    context = query_to_context(query_text=query_text)
+    chat_history[0] = {"role": "system", "content": get_sys_prompt(context=context)}    
     responses = ollama.chat(model, messages=chat_history, stream=True)
     for response in responses:
         yield response['message']['content']
+        
+def init_messages():
+    return [
+        {"role": "system", "content": get_sys_prompt()},
+        {"role": "assistant", "content": welcome_message}
+    ]        
 
-st.title("Chat with " + model)
+st.title("Microwave Witch 🧙‍♀️")
 
-if "selected_model" not in st.session_state:
-    st.session_state.selected_model = ""
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state.messages = init_messages()
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+for message in st.session_state.messages:    
+    if message["role"] != "system":    
         st.markdown(message["content"])
 
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
-   
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        response = st.write_stream(fetch_ollama_replies(st.session_state.messages))
+        response = st.write_stream(generate_reply(st.session_state.messages))
         
     st.session_state.messages.append(
         {"role": "assistant", "content": response})
--- query_qa_pairs.py
+++ query_qa_pairs.py
@@ -10,8 +10,17 @@ def query(query_text, thershold=1.15, n_results=5):
             documents.append((id, distance, document))            
     return documents
 
+def query_to_context(query_text, thershold=1.15, n_results=5):
+    documents = query(query_text=query_text, thershold=thershold, n_results=n_results)
+    context = ""
+    for _, _, document in documents:
+        pairs = document.split('\n')
+        q = pairs[0]
+        a = "\n".join(pairs[1:])
+        context += f"Q: {q}\nA: {a}\n\n"
+    return context
+
 if __name__ == "__main__":
     query_text = input("Enter your question: ")
-    documnents = query(query_text)
-    print(f'total documents: {len(documnents)}')
-    print(documnents)
+    context = query_to_context(query_text)
+    print(context)
