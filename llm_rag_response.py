46f15fc0b6aa49b6fa769d831872a23542fbb701
Paul TK Chan <gnokit@gmail.com>
2024-07-14 09:25:53 +0800

    use top_k and max_distance to control configuration to process result

--- llm_rag_response.py
+++ llm_rag_response.py
@@ -34,6 +34,29 @@ def stream_rag_response(query_text, context):
         yield response["response"]
 
 
+samples = [
+    {
+        "role": "assistant",
+        "content": "\nHi there! I'm the Microwave Witch 🧙\u200d♀️. \nI know all about your microwave oven. What question do you have for me? I'm here to help!\n",
+    },
+    {"role": "user", "content": "hi what's my oven model number?"},
+    {
+        "role": "assistant",
+        "content": " The model numbers for your microwave oven are NN-ST25JW, NN-ST25JB, or NN-ST25JW (mentioned in the context you provided). Please check the identification label on your oven to confirm.",
+    },
+    {"role": "user", "content": "oh how big it is?"},
+    {
+        "role": "assistant",
+        "content": " The size of a household microwave oven varies depending on the model. However, a common size for a standard countertop microwave is around 20 inches (51 cm) wide, 17 inches (43 cm) deep, and 16 inches (40 cm) tall. But please refer to the specifications of your particular model for exact dimensions.",
+    },
+]
+
+
+def to_chat_history(conversation):
+    """Convert conversation to chat history"""
+    pass
+
+
 if __name__ == "__main__":
     query_text = input("What's your question? ")
     context = query_to_context(query_text)
--- query_qa_pairs.py
+++ query_qa_pairs.py
@@ -1,25 +1,32 @@
-from config import chroma_client, Config
+from config import chroma_client, Config, create_logger
+import logging
+
+logger = create_logger("query_qa_pairs")  # Create a logger for this module
 
 # Connect to the Chroma database and get the collection
 collection = chroma_client.get_collection(name=Config["collection"])
 
-def query(query_text, thershold=1.15, n_results=5):
-    """Perform similary search of query_text and return the top n_results documents"""
-    result = collection.query(query_texts=[query_text], n_results=n_results)
+top_k = Config['top_k']
+max_distance = Config['max_distance']
+
+def query(query_text):
+    """Perform similary search of query_text and return the top_k documents"""
+    result = collection.query(query_texts=[query_text], n_results=top_k)
     documents = []
     for id, distance, document in zip(
         result["ids"][0], result["distances"][0], result["documents"][0]
     ):
-        # filter out results with distance greater than Thershold
-        if distance < thershold: 
+        logger.debug("id: {}, distance: {}, document: {}".format(id, distance, document[:20]))
+        # filter out results with distance greater than max_distance
+        if distance < max_distance: 
             documents.append((id, distance, document))
     
     return documents
 
 
-def query_to_context(query_text, thershold=1.15, n_results=5):
+def query_to_context(query_text):
     """Perform similary search of query_text and convert to QA pairs of Q and A"""
-    documents = query(query_text=query_text, thershold=thershold, n_results=n_results)
+    documents = query(query_text=query_text)
     context = ""
     for _, _, document in documents:
         pairs = document.split("\n")
@@ -30,6 +37,7 @@ def query_to_context(query_text, thershold=1.15, n_results=5):
 
 
 if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
     query_text = input("Enter your question: ")
     context = query_to_context(query_text)
     print(context)
